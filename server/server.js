bb7c4250d7c41dbd04af52ca12ecec849b4a827b
Robin <philoin88@gmail.com>
2020-10-10 22:17:13 +0900

    Working on bootcamp API

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 3000;
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// DEV logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Route Files
+const bootcamps = require('./routes/bootcamps');
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+);
+
+// Handle unhandled Promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
